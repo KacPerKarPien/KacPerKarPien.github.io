4eb80c4c62767142ef05905774a11005d64130a4
KacPerKarPien <32983597+KacPerKarPien@users.noreply.github.com>
2018-01-31 06:18:29 +0100

    Update main.js

--- hello-colors/main.js
+++ hello-colors/main.js
@@ -2,7 +2,7 @@ function main(){
     'use strict'
     const icon_menu = document.querySelector("#icon_menu");
     const menu = document.querySelector("#menu");
-    icon_menu.addEventListener("click", ()=>{
+    icon_menu.addEventListener("touchend", ()=>{
         icon_menu.classList.toggle("open_menu_icon");
         menu.classList.toggle("open_menu");
     });
