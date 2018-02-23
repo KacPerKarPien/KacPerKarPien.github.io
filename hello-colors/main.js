c25a8871276b976ab54230b29b59c9bd5c09e443
KacPerKarPien <32983597+KacPerKarPien@users.noreply.github.com>
2018-02-23 20:04:24 +0100

    Add files via upload

--- hello-colors/main.js
+++ hello-colors/main.js
@@ -1,11 +1,16 @@
 function main(){
     'use strict'
-    const icon_menu = document.querySelector("#icon_menu");
+    const iconMenu = document.querySelector("#icon_menu");
     const menu = document.querySelector("#menu");
-    icon_menu.addEventListener("touchend", ()=>{
-        icon_menu.classList.toggle("open_menu_icon");
+    const span = document.querySelectorAll(".button_menu span");
+    iconMenu.addEventListener("click", ()=>{
+        for(let i=0; i<span.length; i++){
+            span[i].classList.toggle(`line-${i+1}`);
+        }
         menu.classList.toggle("open_menu");
     });
-    
 }
 window.addEventListener('load', main);
+
+let a = 10,b = 12, c = a, d = b;
+console.log(c);
